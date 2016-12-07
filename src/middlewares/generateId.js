526caab48ceb1f11f7e6ed430240ec40241aef34
Roman <kaboom.ua@gmail.com>
2016-12-07 22:01:08 +0200

    Update generateId.js

--- src/middlewares/generateId.js
+++ src/middlewares/generateId.js
@@ -1,7 +1,10 @@
 export default store => next => action => {
     const { type, payload } = action
-
+  //лучше придумать более общий способ; через мидлвару будут проходить все экшины, их стоит делать максимально реюзабельными
+  //+ пользуйся константами
   if (type === 'CREATE_COMMENT') {
+    //Плохая логика для генерации id
+    //лучше не мутировать объект
     payload.comment.id = store.getState().comments.last().id + 1
   }
 
